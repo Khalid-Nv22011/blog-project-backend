9438201b56230e58ad3320fe47b5a516cd2245d3
Khalid-Nv22011 <160115222+Khalid-Nv22011@users.noreply.github.com>
2024-04-23 00:42:08 +0000

    user registration API

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db";
 
+// Routes
+
+
 dotenv.config();
 connectDB();
 const app = express();
@@ -10,6 +13,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Server is running...");
 });
+
+
 
 const PORT = process.env.PORT || 5000;
 
